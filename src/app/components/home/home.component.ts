b24c0aca282ad023ce31e84173ac3bde28e5ce8f
Yomar Miranda <mirandayomar@gmail.com>
2018-08-16 18:00:53 -0500

    Mostrando el listado de canciones y sus respectivos artistas en el Home.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from '../../services/spotify.service';
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styles: []
+    selector: 'app-home',
+    templateUrl: './home.component.html',
+    styles: []
 })
 export class HomeComponent implements OnInit {
+    nuevasCanciones: any[] = [];
 
-  constructor() { }
+    constructor(private _spotify: SpotifyService) {
+        this._spotify.getNewReleases()
+            .subscribe((data: any) => {
+                console.log(data.albums.items);
+                this.nuevasCanciones = data.albums.items;
+            });
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
