57349a4bd3a9d76234573c01f888bccdd694bb57
Yomar Miranda <mirandayomar@gmail.com>
2018-08-16 16:33:39 -0500

    Ejemplo: Petición a una API.

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -6,8 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class HomeComponent implements OnInit {
+  paises: any[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.http.get('https://restcountries.eu/rest/v2/lang/es').subscribe( (response: any) => {
+      this.paises = response;
+    });
+  }
 
   ngOnInit() {
   }
