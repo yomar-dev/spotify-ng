31f5ea855de8dfc4b4d12732d4ce6c01bed02e6f
Yomar Miranda <mirandayomar@gmail.com>
2018-08-16 17:32:14 -0500

    Obteniendo información de la API de Spotify.

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class SpotifyService {
 
-  constructor() { }
+    constructor(private http: HttpClient) {
+        const headers = new HttpHeaders({
+            'Authorization': 'Bearer BQAOhP46O9a896KyXJQvRvgA5deCHtvlYXJKpy_Z9yUlGDVUpKIyziMGm79kMMIiz3-FUE8oUkwCEPTUTVk'
+        });
+        this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', { headers })
+            .subscribe(data => {
+                console.log(data);
+            });
+    }
 }
