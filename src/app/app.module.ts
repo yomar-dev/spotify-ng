6a2c62a2d8fd65dbab065614b9f9c9dbdf5412c9
Yomar Miranda <mirandayomar@gmail.com>
2018-08-16 10:13:27 -0500

    Creando el componente Search.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule
