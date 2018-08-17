cd79412d1b165a8b2500aeb5190b3711655a3797
Yomar Miranda <mirandayomar@gmail.com>
2018-08-16 20:45:59 -0500

    Verificación de imagen y pipe para manejar las imagenes.

--- src/app/services/spotify.service.ts
+++ src/app/services/spotify.service.ts
@@ -13,7 +13,7 @@ export class SpotifyService {
     getQuery(query: string){
         const url = `https://api.spotify.com/v1/${query}`;
         const headers = new HttpHeaders({
-            'Authorization': 'Bearer BQAEZAUl6vTomlMfPUouYB-JSulXPhgsBEX6dKgxSkoPT1iOYHiTa21J32K3KCWwSkVDuJsaLlin5FbGnfQ'
+            'Authorization': 'Bearer BQBqAG9-B6iNnNZx0u2zDSEIV763tC60RXOfyldOgCy9oPTZcPP-AQXOVmyhqNxgLLbzCPkv-20Y3jF3OqU'
         });
         return this.http.get(url, { headers });
     }
